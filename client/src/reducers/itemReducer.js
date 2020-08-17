2b0d886e07c0d23dd90b671eb79331d3fef5e4a8
Tim Goens <timcgoens@gmail.com>
2020-08-17 14:04:07 -0500

    finished up my mern project

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,14 @@
 import { v1 as uuid } from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Eggs" },
-    { id: uuid(), name: "Milk" },
-    { id: uuid(), name: "Steak" },
-    { id: uuid(), name: "Feet" },
-  ],
+  items: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -15,17 +16,27 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
-    case DELETE_ITEM:
-      return {
-        ...state,
-        items: state.items.filter((item) => item.id !== action.payload),
-      };
+
     case ADD_ITEM:
       return {
         ...state,
         //this takes your payload(entire item) and then adds it to your existing state
         items: [action.payload, ...state.items],
+      };
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item._id !== action.payload),
+      };
+
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
